10a2acbebcd5f503e974e00af9c344cf10d72a9f
Yulia Serheieva <Julia.sergeeva93@gmail.com>
2024-04-17 04:09:19 +0300

    correction import yup

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,6 +1,6 @@
 import { nanoid } from "nanoid";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import * as Yup from "Yup";
+import * as Yup from "yup";
 
 import style from "./ContactForm.module.css";
 
